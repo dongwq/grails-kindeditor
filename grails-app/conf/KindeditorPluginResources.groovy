fddbfdbb33199ab8b1041809e4bbde8612c68a6f
dongwq <dongwqs@gmail.com>
2013-06-11 18:17:04 +0800

    disable bundle for css loaded by js.

--- grails-app/conf/KindeditorPluginResources.groovy
+++ grails-app/conf/KindeditorPluginResources.groovy
@@ -1,12 +1,14 @@
 modules = {
 
     'kindeditor' {
+        defaultBundle(false)
         resource url: 'ke-zh/kindeditor-min.js'
         resource url: 'ke-zh/skins/default.css'
         resource url: 'ke-zh/skins/common/editor.css'
     }
 
     'kindeditor-dev' {
+        defaultBundle(false)
         resource url: 'ke-zh/kindeditor.js'
         resource url: 'ke-zh/skins/default.css'
         resource url: 'ke-zh/skins/common/editor.css'
